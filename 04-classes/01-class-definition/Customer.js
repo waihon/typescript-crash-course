a2c2094ef3110877fe4387abdf96428a4bac24a1
Waihon Yew <yewwaihon@gmail.com>
2022-01-13 23:32:14 +0800

    Modify Customer class to add a constructor

--- 04-classes/01-class-definition/Customer.js
+++ 04-classes/01-class-definition/Customer.js
@@ -1,10 +1,10 @@
 var Customer = /** @class */ (function () {
-    function Customer() {
+    function Customer(theFirst, theLast) {
+        this.firstName = theFirst;
+        this.lastName = theLast;
     }
     return Customer;
 }());
-var myCustomer = new Customer();
-myCustomer.firstName = "Martin";
-myCustomer.lastName = "Dixon";
+var myCustomer = new Customer("Martin", "Dixon");
 console.log(myCustomer.firstName);
 console.log(myCustomer.lastName);
--- 04-classes/01-class-definition/Customer.ts
+++ 04-classes/01-class-definition/Customer.ts
@@ -1,11 +1,14 @@
 class Customer {
   firstName: string;
   lastName: string;
+
+  constructor(theFirst: string, theLast: string) {
+    this.firstName = theFirst;
+    this.lastName = theLast;
+  }
 }
 
-let myCustomer = new Customer();
-myCustomer.firstName = "Martin"
-myCustomer.lastName = "Dixon"
+let myCustomer = new Customer("Martin", "Dixon");
 
 console.log(myCustomer.firstName);
 console.log(myCustomer.lastName);
