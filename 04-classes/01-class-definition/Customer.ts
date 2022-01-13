fe73e4c79202001f6cf146227cb1503d9d1f4bd1
Waihon Yew <yewwaihon@gmail.com>
2022-01-13 23:28:59 +0800

    Create a simple Customer class with 2 properties

---
+++ 04-classes/01-class-definition/Customer.ts
@@ -0,0 +1,11 @@
+class Customer {
+  firstName: string;
+  lastName: string;
+}
+
+let myCustomer = new Customer();
+myCustomer.firstName = "Martin"
+myCustomer.lastName = "Dixon"
+
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
